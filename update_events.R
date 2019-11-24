0fa1c0cca029b5a9da5f3c1219f65df9a3f330a4
KasiaWo <kaswoz4@gmail.com>
2019-11-24 22:02:30 +0100

    template with database

--- update_events.R
+++ update_events.R
@@ -1,41 +1,47 @@
 library(lubridate)
 library(dplyr)
 
-event_database <- data.frame(date = c('12-11-2019', '18-11-2019', '20-12-2019', '10-01-2020'),
-                             person = c('prof. Jan Mielniczuk', 'Tomasz Stanisławek', 'Tomasz Stanisławek', 'Tomasz Stanisławek'),
-                             title = c('Nowe problemy matematyczne w uczeniu maszynowym', 'prace z CoNLP', 'temat nowy', 'temat nowszy'),
-                             location = c('s.102 MiNI', 's.44 MiNI', 's.44 MiNI', 's.44 MiNI'))
+# event_database <- data.frame(date = c('12-11-2019', '18-11-2019', '25-11-2019',  '28-10-2019'),
+#                              person = c('prof. Jan Mielniczuk', 'Tomasz Stanisławek', 'Barbara Rychalska', 'dr Ewa Strzałkowska-Kominiak'),
+#                              title = c('Nowe problemy matematyczne w uczeniu maszynowym', 'prace z CoNLP', 'prace z ACL', 'Wybrane zagadnienia analizy przeżycia'),
+#                              location = c('s.102 MiNI', 's.44 MiNI', 's.44 MiNI', 's.40 MiNI'))
+# write.csv(event_database, './event_database.csv')
 
+update_event_database <- function(event_database, date, person, title, location){
+  row_to_update <- data.frame(date = date, person = person, title = title, location = location )
+  updated_event_database <- rbind(event_database, row_to_update)
+  
+  event_database <- unique(event_database)
+  return(event_database)
+}
+
+
+event_database <- read.csv('./event_database.csv')
+
+
+
+
+year_sel <- 2019
+month_sel <- 11
 
 event_database$date <- as.Date(event_database$date, format = '%d-%m-%Y')
 
-for(year_sel in unique(year(event_database$date))){
+
   tmp1 <- event_database %>% 
     filter(year(date) == year_sel)
   
-  for(month_sel in sort(unique(month(tmp1$date)))){
+
     tmp <- tmp1 %>% 
       filter( month(date) == month_sel)
     print(tmp)
     
     rmarkdown::render(input = './template_post.Rmd',
                       output_dir = './docs',
-                      output_file = paste0('events_', year_sel, '_', month_sel, '.html'),
-                      #output_file = path.expand(paste0(getwd(),'/content/post/events_', year_sel, '_', month_sel, '.html')),
-                      params = list(data = tmp,
+                      output_file = 'index.html',
+                     params = list(data = tmp,
                                     set_title = paste0('Wydarzenia na MiNI ', year_sel, " ", month_sel),
                                     #set_slug = paste0('Post ', year_sel, " ", month_sel),
                                     set_date = Sys.Date()),
                       encoding = 'UTF-8')
     
-    # slug <- paste0('Post ', year_sel, " ", month_sel)
-    # date <- Sys.Date()
-    # data <- tmp
-    # title <- paste0('Wydarzenia na MiNI ', year_sel, " ", month_sel)
-    # 
-    # #file.remove(paste0('./content/post/events_', year_sel, '_', month_sel, '.html'))
-    # 
-    # knitr::knit(input = './R/post_template_2.Rmd',
-    #             output = paste0('./content/post/events_', year_sel, '_', month_sel, '_2.md'))
-  }
-}
+    
